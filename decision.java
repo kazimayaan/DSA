3d7f932bbbd2b923380ead11340022aae8fa810f
kazimayaan <kazimayaan486@gmail.com>
2022-09-06 21:05:12 +0530

    if and while

---
+++ decision.java
@@ -0,0 +1,18 @@
+public class decision {
+
+    public static void main(String[] args) {
+        if (true){
+            System.out.println("hello world");
+        }
+
+        int a = 10;
+        int count = 1;
+        while(count<a+1){
+            System.out.println("this is while loop iteration: " + count);
+            count= count+1;
+        }
+        System.out.println("ok byeeeee");
+
+    }
+    
+}
