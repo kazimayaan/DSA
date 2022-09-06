2414c22f5c3194ebbc9ba069007ef30a152c1b90
kazimayaan <kazimayaan486@gmail.com>
2022-09-06 21:10:07 +0530

    for loops

---
+++ loops.java
@@ -0,0 +1,10 @@
+public class loops {
+    public static void main(String[] args) {
+        
+        System.out.println("lets learn for loops");
+
+        for(int i = 0; i< 10; i++){
+            System.out.println("this is " + i + "th iteration");
+        }
+    }
+}
