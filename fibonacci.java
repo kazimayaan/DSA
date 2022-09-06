86a9bbe288cbfcfc43552f8cc6fb462943d9fb80
kazimayaan <kazimayaan486@gmail.com>
2022-09-07 00:22:23 +0530

    fibonacci with for and while loops

---
+++ fibonacci.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class fibonacci {
+    public static void main(String[] args) {
+
+        int a = 0;
+        int b= 1;
+        int k= 2;
+        Scanner in = new Scanner(System.in);
+        int n= in.nextInt();
+        for (int c = 2; c <=n; c++) {
+
+         int temp= b;
+         b= a+b;
+         a=temp;
+
+        }
+        System.out.println(b);
+
+        int p= 0 ;
+        int i= 1;
+        int count=2;
+        while(count<=n){
+            int temp = i;
+            i = i+p;
+            p = temp;
+            count++;
+        }
+        System.out.println(i);
+        in.close();
+    }
+
+}
