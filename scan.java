37f190861fafa4b8937642cbd6b947a98a86cf29
kazimayaan <kazimayaan486@gmail.com>
2022-09-06 20:06:51 +0530

    input and output streams

---
+++ scan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class scan {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hey How are you");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+        input.close();
+
+    }
+
+}
