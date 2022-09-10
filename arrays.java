f2273405849032cf72da011be365a456cf3db81f
kazimayaan <kazimayaan486@gmail.com>
2022-09-10 17:47:14 +0530

    learnt more

--- arrays.java
+++ arrays.java
@@ -10,6 +10,19 @@ public class arrays {
         int rolls[]= {1,2,3,4,5};
         // array[] kazim = new arrays();
         int [] rnos = new int[5];
-        
+        rnos[1] = 23;
+     
+        System.out.println(rnos[1]);
+        //arrays are mutable'
+        // /new keyword/ to create a new object
+        System.out.println(rnos[0]);
+        for (int i = 0; i < rnos.length; i++) {
+            System.out.println(rnos[i]);
+        }
+        String[] array = new String[5];
+        System.out.println(array[1]);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
