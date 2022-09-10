974a59de5d6230862495ded0f5cd08eb17fd37b7
kazimayaan <kazimayaan486@gmail.com>
2022-09-10 19:08:46 +0530

    aeeayslist

--- arraylist.java
+++ arraylist.java
@@ -1,8 +1,29 @@
+import java.rmi.server.SocketSecurityException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class arraylist {
     public static void main(String[] args) {
         ArrayList<Integer> kazim = new ArrayList<>();
-        
+        Scanner in = new Scanner(System.in);
+
+        // kazim.add(444);
+        // kazim.add(444);
+        // kazim.add(444);
+        // kazim.add(444);
+        // kazim.add(444);
+        // kazim.add(444);
+        // kazim.add(444);
+        for (int i = 0; i < 10; i++) {
+            int e = in.nextInt();
+            kazim.add(e);
+        }
+        for (int i = 0; i <10; i++) {
+            // SocketSecurityException
+            System.out.println(kazim.get(i));
+        }
+        // System.out.println(kazim.addAll(kazim));
+
+
     }
 }
