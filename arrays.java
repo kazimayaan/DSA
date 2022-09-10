031e47c4df01ac802d1a7917ba169f223a86fe9d
kazimayaan <kazimayaan486@gmail.com>
2022-09-10 17:37:29 +0530

    ararys

---
+++ arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+    public static void main(String[] args) {
+        // q store a roll no
+        int a = 189;
+        // store a name
+        String name = "kazim";
+
+        // q store 5 roll nos;
+
+        int rolls[]= {1,2,3,4,5};
+        // array[] kazim = new arrays();
+        int [] rnos = new int[5];
+        
+    }
+}
