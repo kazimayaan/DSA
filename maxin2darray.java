550d4a879754e5e77cbcbfa82534d0084b05f3eb
kazimayaan <kazimayaan486@gmail.com>
2022-09-11 16:16:08 +0530

    max item in the 2d array

---
+++ maxin2darray.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class maxin2darray {
+// public class searchin2darray {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 32, 34},
+            {33, 55, 56},
+            {33,44,445},
+            {12 , 33, 45544}
+        };
+        // int target = 445;
+
+       int ans =  search(arr);
+    //    System.out.println(arr[ans]);
+    
+    //    System.out.println(Arrays.toString(ans));
+    System.out.println(ans);
+    }
+
+    private static int search(int[][] arr) {
+        // boolean flag = false;
+        int target = arr[0][0];
+        for (int row = 0; row < arr.length; row++) {
+
+            for (int j = 0; j < 3; j++) {
+                    if(arr[row][j]>target){
+                        target = arr[row][j];
+                    }
+                
+            }
+            
+        }
+        return target;
+    }
+// }/
+
+}
