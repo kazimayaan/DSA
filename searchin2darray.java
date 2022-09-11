f1d3f6acb22276f2e2a3a519a2a72fab1300968b
kazimayaan <kazimayaan486@gmail.com>
2022-09-11 16:11:36 +0530

    2d arrays q

---
+++ searchin2darray.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class searchin2darray {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 32, 34},
+            {33, 55, 56},
+            {33,44,445},
+            {12 , 33, 455}
+        };
+        int target = 445;
+
+       int[] ans =  search(arr,target);
+       System.out.println(Arrays.toString(ans));
+    }
+
+    private static int[] search(int[][] arr, int target) {
+        // boolean flag = false;
+        for (int row = 0; row < arr.length; row++) {
+
+            for (int j = 0; j < 3; j++) {
+                    if(arr[row][j]==target){
+                        return new int[] {row,j};
+                    }
+                
+            }
+            
+        }
+        return new int[] {-1,-1};
+    }
+}
