961e7453aa400114e90ba583313f5e50e8d18c18
kazimayaan <kazimayaan486@gmail.com>
2022-09-17 21:28:09 +0530

    removed resource leakage

---
+++ binaryq.java
@@ -0,0 +1,37 @@
+public class binaryq {
+    //cielimg of a number: 
+    //sorted array: use binary search
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,10,66,76,86,92};
+        int target = 5;
+       int index =  cieling(arr,target);
+       System.out.println(arr[index]);
+        
+    }
+
+    private static int cieling(int[] arr, int target) {
+
+        int s = 0;
+        int e = arr.length -1;
+       while(s<=e){
+        int mid = (s+e)/2;
+        if(arr[mid]==target){
+        return mid;
+
+        }
+        else if(arr[mid]<target){
+            s= mid+1;
+
+        }
+        else{
+           
+           
+            e=mid-1;
+        }
+
+       }
+       return s;
+    }
+   
+}
+
