5a08cb99dc30e754246552a223ae7935e6a71e41
kazimayaan <kazimayaan486@gmail.com>
2022-09-17 21:46:03 +0530

    removed exceptions

--- binaryq.java
+++ binaryq.java
@@ -3,9 +3,11 @@ public class binaryq {
     //sorted array: use binary search
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,10,66,76,86,92};
-        int target = 5;
+        int target = 93;
+   
        int index =  cieling(arr,target);
        System.out.println(arr[index]);
+       
         
     }
 
@@ -13,6 +15,10 @@ public class binaryq {
 
         int s = 0;
         int e = arr.length -1;
+        if(target>arr[arr.length-1]){
+            System.out.println("element exceeds limits");
+            return -1;
+        }
        while(s<=e){
         int mid = (s+e)/2;
         if(arr[mid]==target){
