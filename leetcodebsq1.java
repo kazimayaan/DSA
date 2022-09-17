d6cb6573bdcbcba9e89e83a68c1e9b713113ede3
kazimayaan <kazimayaan486@gmail.com>
2022-09-17 22:47:09 +0530

    solved problem

---
+++ leetcodebsq1.java
@@ -0,0 +1,23 @@
+public class leetcodebsq1 {
+    public static void main(String[] args) {
+        char[] arr = {'a','b','d','k','m','n'};
+        char target = 'z';
+        char res = lol(arr,target);
+        System.out.println(res);
+    }
+
+    private static char lol(char[] arr, char target) {
+        int s = 0;
+        int e = arr.length-1;
+        while(s<=e){
+            int mid = (s+e)/2;
+            if(arr[mid]<target){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return arr[s%arr.length];
+    }
+}
