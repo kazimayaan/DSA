427a653c0dfc7a9eed3d305a465e20f44c91818b
kazimayaan <kazimayaan486@gmail.com>
2022-10-08 22:14:35 +0530

    idk

--- BSQ11.java
+++ BSQ11.java
@@ -1,5 +1,14 @@
 public class BSQ11 {
     public static void main(String[] args) {
         //subarray question
+        //minimum no iof partitions we can make : is 1;
+        // the maximum number of partitions that can be: it is m;
+        // what will be ans in case 1
+        //case 1 ans is sum of the array itself
+        // case 2 answer is the manimum of the array
+        //max is case1 
+        //min is case2 
+// min calue is max value in array
+//max is sum of values all in array
     }
 }
