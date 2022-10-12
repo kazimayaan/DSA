788dba71c5d5704f6856db4bba5f596797f6e972
kazimayaan <kazimayaan486@gmail.com>
2022-10-12 11:31:08 +0530

    changes

--- strictlysorted2darray.java
+++ strictlysorted2darray.java
@@ -1,5 +1,16 @@
 public class strictlysorted2darray {
     public static void main(String[] args) {
-        
+        int[][] arr = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        int target = 12;
+        int[] sol = search(arr,target);
+    }
+
+    private static int[] search(int[][] arr, int target) {
+        return null;
     }
 }
