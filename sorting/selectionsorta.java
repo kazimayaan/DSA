4f34f767f02b29c1a06cb77a2f2ecb25ea7a145d
kazimayaan <kazimayaan486@gmail.com>
2022-10-13 20:47:41 +0530

    formatted document

--- sorting/selectionsorta.java
+++ sorting/selectionsorta.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class selectionsorta {
     public static void main(String[] args) {
-        int[] arr = {1,23,4,5,3,77,9,7};
+        int[] arr = { 1, 23, 4, 5, 3, 77, 9, 7 };
         selections(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -12,10 +12,10 @@ public class selectionsorta {
     private static void selections(int[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
-            int last = arr.length -1-i;
-            int maxIn = getMaxIndex(arr,0,last);
-            
-            swap(arr,maxIn,last);
+            int last = arr.length - 1 - i;
+            int maxIn = getMaxIndex(arr, 0, last);
+
+            swap(arr, maxIn, last);
         }
 
     }
@@ -23,13 +23,13 @@ public class selectionsorta {
     private static void swap(int[] arr, int maxIn, int last) {
         int temp = arr[maxIn];
         arr[maxIn] = arr[last];
-        arr[last ] = temp;
+        arr[last] = temp;
     }
 
     private static int getMaxIndex(int[] arr, int i, int last) {
-        int maxIn=0;
+        int maxIn = 0;
         for (int j = 0; j <= last; j++) {
-            if(arr[maxIn]<arr[j]){
+            if (arr[maxIn] < arr[j]) {
                 maxIn = j;
             }
         }
