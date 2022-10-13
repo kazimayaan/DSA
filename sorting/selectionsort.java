7aba5c90169180db77a331f9b15a00bf19bb36e8
kazimayaan <kazimayaan486@gmail.com>
2022-10-13 14:13:17 +0530

    selection sortn

---
+++ sorting/selectionsort.java
@@ -0,0 +1,15 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class selectionsort {
+    public static void main(String[] args) {
+ int[] arr = {1,5,6,7,3,2,4,10,9};
+selectinsort(arr);
+ System.out.println(Arrays.toString(arr));
+    }
+
+    private static void selectinsort(int[] arr) {
+        
+    }
+}
