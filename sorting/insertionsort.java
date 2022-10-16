b682f2cd26976a5143d8ca85a8f4d9cf8bb1c869
kazimayaan <kazimayaan486@gmail.com>
2022-10-16 13:31:06 +0530

    my own code

--- sorting/insertionsort.java
+++ sorting/insertionsort.java
@@ -1,11 +1,33 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class insertionsort {
 
     public static void main(String[] args) {
         
-         
-        
+         int[] arr= {11,1,3,4,2,77,65,44,45,6};
+
+         insertionsort(arr);
+         System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void insertionsort(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+                else{
+                    break;
+                }
+            }
+            
+        }
     }
     
 }
