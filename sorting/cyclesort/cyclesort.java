11d93e17f2db50eb1a330fcd2740303fad563ef1
kazimayaan <kazimayaan486@gmail.com>
2022-10-16 16:22:43 +0530

    iterating thru array

--- sorting/cyclesort/cyclesort.java
+++ sorting/cyclesort/cyclesort.java
@@ -1,10 +1,22 @@
 package sorting.cyclesort;
 
+import java.util.Arrays;
+
 public class cyclesort {
     public static void main(String[] args) {
         // when given numbers from range 1 to N => use cyclic sort
         // 1 to N is cyclic sort
         // 
-        
+        // we need only n comparisons in cyclic sort
+        // /very veyr very very // important 
+        int[] arr = {8,7,6,5,4,3,2,1};
+        cyclesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void cyclesort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            
+        }
     }
 }
