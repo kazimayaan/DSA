6585baa2c535d3c9ae094e0b7b3b3e8a9d9bfb29
kazimayaan <kazimayaan486@gmail.com>
2022-10-16 16:10:45 +0530

    pro tip

--- sorting/cyclesort/cyclesort.java
+++ sorting/cyclesort/cyclesort.java
@@ -3,5 +3,7 @@ package sorting.cyclesort;
 public class cyclesort {
     public static void main(String[] args) {
         // when given numbers from range 1 to N => use cyclic sort
+        // 1 to N is cyclic sort
+        // 
     }
 }
