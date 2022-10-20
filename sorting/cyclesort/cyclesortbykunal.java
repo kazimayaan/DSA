de779ac4f3600260bf555b7d9687774334f76f65
kazimayaan <kazimayaan486@gmail.com>
2022-10-20 20:24:47 +0530

    changeds

--- sorting/cyclesort/cyclesortbykunal.java
+++ sorting/cyclesort/cyclesortbykunal.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class cyclesortbykunal {
     public static void main(String[] args) {
         // int[] arr = {1,2,4,3,8,7,6,9,5};
-        int[] arar = {};
+        int[] arar = {1,2,3};
 
         cyclesortbykkunal(arar);
         System.out.println(Arrays.toString(arar));
