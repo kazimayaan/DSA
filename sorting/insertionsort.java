5f0f7062f247ebdee256af0ff5852027a38b30a6
kazimayaan <kazimayaan486@gmail.com>
2022-10-20 20:25:30 +0530

    indentation correction

--- sorting/insertionsort.java
+++ sorting/insertionsort.java
@@ -5,30 +5,30 @@ import java.util.Arrays;
 public class insertionsort {
 
     public static void main(String[] args) {
-        
-         int[] arr= {11,1,3,4,2,77,65,44,45,6};
 
-         oinsertionsort(arr);
-         System.out.println(Arrays.toString(arr));
+        int[] arr = { 11, 1, 3, 4, 2, 77, 65, 44, 45, 6 };
+
+        oinsertionsort(arr);
+        System.out.println(Arrays.toString(arr));
 
     }
 
     private static void oinsertionsort(int[] arr) {
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
 
-            for (int j = i+1; j >0; j--) {
-                if(arr[j]<arr[j-1]){
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+
                     int temp = arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp; 
-                    
-                }
-                else{
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+
+                } else {
                     break;
                 }
             }
-            
+
         }
     }
-    
+
 }
