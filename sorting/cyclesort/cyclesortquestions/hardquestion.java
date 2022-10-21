941999c26ad6d49ad07e912b500edd016f8ce129
kazimayaan <kazimayaan486@gmail.com>
2022-10-21 20:29:20 +0530

    changing input

--- sorting/cyclesort/cyclesortquestions/hardquestion.java
+++ sorting/cyclesort/cyclesortquestions/hardquestion.java
@@ -7,7 +7,7 @@ public class hardquestion {
         
 
 
-        int[] arr = {1,2,3,9};
+        int[] arr = {-1,2,3,9};
 
         int sol = firstMissingPositive(arr);
         System.out.println(sol);
