192bf4dcd89d0c4eac7a20e5dbde8b634a6acc4c
kazimayaan <kazimayaan486@gmail.com>
2022-10-21 20:11:25 +0530

    input

--- sorting/cyclesort/cyclesortquestions/hardquestion.java
+++ sorting/cyclesort/cyclesortquestions/hardquestion.java
@@ -1,5 +1,13 @@
 package sorting.cyclesort.cyclesortquestions;
 
 public class hardquestion {
-    
+    // first missing Positive
+
+    public static void main(String[] args) {
+        
+
+
+        int[] arr = {1,2,3,9};
+        
+    }
 }
