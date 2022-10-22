3c169d88c670a0fce6d0e33e2ee2c1dde1a81d52
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 15:58:07 +0530

    exp

--- strings/first.java
+++ strings/first.java
@@ -3,7 +3,10 @@ package strings;
 public class first {
     // String si most commonly used class in java
     // every string we create is an objecy, the name is referenve variable
-    // referenve points point to same object always.
+    // referenve points point to some object always.
+    // internally :_
+    // string pool : inyernallu yhe string its a seperate memory structure for stringsiin heap memory
+
     public static void main(String[] args) {
         String kazim = "KAZIM";
         System.out.println(kazim);
