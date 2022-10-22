e7c8b93476c224a6b067500f70f59891dacb6985
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:54:10 +0530

    soltuoon forund

--- strings/stringbuilderf.java
+++ strings/stringbuilderf.java
@@ -2,6 +2,10 @@ package strings;
 
 public class stringbuilderf {
     public static void main(String[] args) {
-        
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            char ch = (char)('a'+1);
+            builder.append(ch);
+        }
     }
 }
