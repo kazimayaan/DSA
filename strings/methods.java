389c94de7d86e48dcd11cf7d5cb5608312ab2100
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:57:14 +0530

    name

--- strings/methods.java
+++ strings/methods.java
@@ -2,6 +2,7 @@ package strings;
 
 public class methods {
     public static void main(String[] args) {
-        
+        String name = "Kazim Ayaan P S";
+        System.out.println(name.toCharArray());
     }
 }
