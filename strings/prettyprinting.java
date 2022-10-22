4f9a08848aa7403855bd89927796ffd0e18afb86
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:27:25 +0530

    pp

--- strings/prettyprinting.java
+++ strings/prettyprinting.java
@@ -1,5 +1,9 @@
 package strings;
 
 public class prettyprinting {
-    
+    public static void main(String[] args) {
+        float a = 45.2313f;
+        System.out.println(a);
+        
+    }
 }
