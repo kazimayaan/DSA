bf05209fe68a7d6883000589453c1dcb0664bacc
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:32:30 +0530

    operators wokrinf

--- strings/operators.java
+++ strings/operators.java
@@ -2,6 +2,9 @@ package strings;
 
 public class operators {
     public static void main(String[] args) {
-        
+        System.out.println('a'+'b');
+        // characters : converts into ascii values and converts it adds
+        System.out.println("a"+ "b");
+        // with string it is ponly concatenating
     }
 }
