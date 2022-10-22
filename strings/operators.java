9ee4be647bd0c93efe4b0f64a59a5abd6aff3100
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:33:17 +0530

    wokrinf

--- strings/operators.java
+++ strings/operators.java
@@ -6,5 +6,7 @@ public class operators {
         // characters : converts into ascii values and converts it adds
         System.out.println("a"+ "b");
         // with string it is ponly concatenating
+        System.out.println('a'+3);
+        System.out.println('A'+1);
     }
 }
