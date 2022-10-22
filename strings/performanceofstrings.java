cb4bc39ddba307793b675594fcf30b7ce94eaca5
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:46:23 +0530

    char

--- strings/performanceofstrings.java
+++ strings/performanceofstrings.java
@@ -7,8 +7,11 @@ public class performanceofstrings {
   
     public static void main(String[] args) {
         String series = "";
-        for (int i = 0; i < 5; i++) {
-            s
+        for (int i = 0; i < 26; i++) {
+            char ch = (char)('a'+i);
+            System.out.println(ch);
+            series = series +ch;
         }
+        System.out.println(series);
     }
 }
