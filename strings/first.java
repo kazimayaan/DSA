6d28e50f8755a9b152af539223fc2e76176b4f33
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 15:55:09 +0530

    comment

--- strings/first.java
+++ strings/first.java
@@ -1,6 +1,7 @@
 package strings;
 
 public class first {
+    // String si most commonly used class in java
     public static void main(String[] args) {
         String kazim = "KAZIM";
         System.out.println(kazim);
