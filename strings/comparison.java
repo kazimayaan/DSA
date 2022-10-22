cf5d4913352db0bc06b676f37eee2c89b3de6ecf
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:04:21 +0530

    comaorison of strings

--- strings/comparison.java
+++ strings/comparison.java
@@ -2,6 +2,10 @@ package strings;
 
 public class comparison {
     public static void main(String[] args) {
-        
+        String a = "kazim";
+        String b = "kazim";
+
+        System.out.println(a==b);
+
     }
 }
