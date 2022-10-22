994b6705933afd5ea137dce97bd606cc9de037e0
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:13:46 +0530

    checking print methods

--- strings/output.java
+++ strings/output.java
@@ -1,7 +1,13 @@
 package strings;
 
+import java.lang.reflect.Array;
+
 public class output {
     public static void main(String[] args) {
-        
+        // function overloading
+        // same name of functions but becs of parameter it changes the imple,mentation
+        System.out.println(56);
+        System.out.println("kazim");
+        System.out.println(new int[]{1,23,45});
     }
 }
