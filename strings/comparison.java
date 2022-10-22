fc778126ef846d865ba584e1d16848bfee753491
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:08:51 +0530

    indentation change

--- strings/comparison.java
+++ strings/comparison.java
@@ -5,16 +5,16 @@ public class comparison {
         String a = "kazim";
         String b = "kazim";
 
-        System.out.println(a==b);
-// == method : comparator
-// == checks both values and reference variabkles to same object
-// because it results true, the red var a , b are pointing to same object kazim;
-// how to create a new different hnew objectsl
-String c = new String("kazim");
-System.out.println(b==c);
-// values are same, but the object is diff so faklse
-// when we need ro check for values, use l.equals method
-System.out.println(b.equals(c));
-// gave true becs it cares about only the valuea
+        System.out.println(a == b);
+        // == method : comparator
+        // == checks both values and reference variabkles to same object
+        // because it results true, the red var a , b are pointing to same object kazim;
+        // how to create a new different hnew objectsl
+        String c = new String("kazim");
+        System.out.println(b == c);
+        // values are same, but the object is diff so faklse
+        // when we need ro check for values, use l.equals method
+        System.out.println(b.equals(c));
+        // gave true becs it cares about only the valuea
     }
 }
