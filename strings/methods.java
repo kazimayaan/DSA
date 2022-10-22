08eaa311c8689e66a301a1d896b90f12f745b63f
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 17:03:13 +0530

    methods exploration

--- strings/methods.java
+++ strings/methods.java
@@ -1,8 +1,20 @@
 package strings;
 
+import java.util.Arrays;
+
 public class methods {
     public static void main(String[] args) {
         String name = "Kazim Ayaan P S";
-        System.out.println(name.toCharArray());
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toLowerCase());
+        System.out.println(name);
+        System.out.println(name.toUpperCase());
+       int n =  name.length();
+       System.out.println(n);
+       System.out.println(name.indexOf('a'));
+       System.out.println(name.isEmpty());
+       System.out.println(name.lastIndexOf('a'));
+      String k =  "                 kazim             ";
+        System.out.println(k.strip());
     }
 }
