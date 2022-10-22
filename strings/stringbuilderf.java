c062054523b37075acb5a4423e12380e6bc895c1
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:54:45 +0530

    indentation

--- strings/stringbuilderf.java
+++ strings/stringbuilderf.java
@@ -4,7 +4,7 @@ public class stringbuilderf {
     public static void main(String[] args) {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < 26; i++) {
-            char ch = (char)('a'+i);
+            char ch = (char) ('a' + i);
             builder.append(ch);
         }
         System.out.println(builder);
