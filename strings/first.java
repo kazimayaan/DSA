4364c18f06383d6066120114d0fe1a55523e57aa
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 15:54:24 +0530

    printing name

--- strings/first.java
+++ strings/first.java
@@ -4,5 +4,7 @@ public class first {
     public static void main(String[] args) {
         String kazim = "KAZIM";
         System.out.println(kazim);
+        String name = "kazim Ayaan P S";
+        System.out.println(name);
     }
 }
