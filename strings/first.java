4b072ab3ebde18f1590e6f595d14e93ff5685c2c
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 15:55:52 +0530

    explaination

--- strings/first.java
+++ strings/first.java
@@ -2,6 +2,8 @@ package strings;
 
 public class first {
     // String si most commonly used class in java
+    // every string we create is an objecy, the name is referenve variable
+    // 
     public static void main(String[] args) {
         String kazim = "KAZIM";
         System.out.println(kazim);
