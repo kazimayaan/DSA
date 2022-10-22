747b468c16da31cb48ccccaa076314a12c677ad0
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 16:29:55 +0530

    nah n ot working

--- strings/prettyprinting.java
+++ strings/prettyprinting.java
@@ -4,6 +4,9 @@ public class prettyprinting {
     public static void main(String[] args) {
         float a = 45.2313f;
         System.out.println(a);
+        // System.out.println("%f",a);
+        System.out.println(Math.PI);
+        // System.out.println("hello my name os %s and i am %s","kazim","cool");
         
     }
 }
