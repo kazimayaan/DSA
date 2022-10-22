e733abd7382e8e039db2359dea8d2b2433a7fc21
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 15:56:40 +0530

    stack and heap memeory

--- strings/first.java
+++ strings/first.java
@@ -3,7 +3,7 @@ package strings;
 public class first {
     // String si most commonly used class in java
     // every string we create is an objecy, the name is referenve variable
-    // 
+    // referenve points point to same object always.
     public static void main(String[] args) {
         String kazim = "KAZIM";
         System.out.println(kazim);
