e119d4b1836826f9805a39d61d3ad5458a771a47
kazimayaan <kazimayaan486@gmail.com>
2022-10-22 17:06:10 +0530

    small questiomn

---
+++ strings/palindrome.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class palindrome {
+    public static void main(String[] args) {
+        String pali = "aaaa";
+        Boolean sol = palindrome(pali);
+    }
+
+    private static Boolean palindrome(String pali) {
+        return null;
+    }
+}
