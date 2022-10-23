9897ba6692f3e6950a659eab3df679008b4f7901
kazimayaan <kazimayaan486@gmail.com>
2022-10-23 16:26:08 +0530

    process

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -3,5 +3,6 @@ package patterns;
 public class pattern {
     public static void main(String[] args) {
         // learning in public
+        // patterns are important because it enhances your thoiught process
     }
 }
