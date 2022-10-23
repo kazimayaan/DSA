4d4b4c9959e8b3db04963a9bd3a7c97b75293915
kazimayaan <kazimayaan486@gmail.com>
2022-10-23 12:55:16 +0530

    prettier

--- sorting/questions/ranktransform.java
+++ sorting/questions/ranktransform.java
@@ -2,29 +2,30 @@ package sorting.questions;
 
 import java.util.Arrays;
 
-public  class ranktransform {
+public class ranktransform {
     public static void main(String[] args) {
-        int[] arr = {1,5,3};
+        int[] arr = { 1, 5, 3 };
 
-       int[] sol =  arrayRankTransform(arr);
+        int[] sol = arrayRankTransform(arr);
         System.out.println(Arrays.toString(sol));
     }
+
     public static int[] arrayRankTransform(int[] arr) {
         Arrays.sort(arr);
         int[] res = new int[arr.length];
         int k = 1;
-        for(int i = 0; i > arr.length-1; i++){
-           
+        for (int i = 0; i > arr.length - 1; i++) {
+
             int min = 0;
-            
-            for(int j = 1; j <arr.length ; j++){
-                if(arr[min]>arr[j]){
-                min = j;
-            }
-                res[min]=k;
+
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+                res[min] = k;
                 k++;
             }
-            
+
         }
         return res;
     }
