b026c639f6bdef4f5e2715abe9c3a56d02d22c2c
kazimayaan <kazimayaan486@gmail.com>
2022-10-23 13:48:46 +0530

    commit msddg

--- basics/basics/conditions_loops.java
+++ basics/basics/conditions_loops.java
@@ -1,3 +1,4 @@
+package basics;
 import java.util.Scanner;
 
 // import javax.lang.model.util.ElementScanner14;
--- basics/basics/types.java
+++ basics/basics/types.java
@@ -1,3 +1,4 @@
+package basics;
 public class types {
 
     public static void main(String[] args) {
