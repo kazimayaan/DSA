b9b21da15711da43bcb12176bd39689f1b3a87b2
kazimayaan <kazimayaan486@gmail.com>
2022-10-23 16:26:31 +0530

    foundation

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -4,5 +4,7 @@ public class pattern {
     public static void main(String[] args) {
         // learning in public
         // patterns are important because it enhances your thoiught process
+        // pattern questions no one asks much
+        
     }
 }
