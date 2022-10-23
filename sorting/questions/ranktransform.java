06760a8e8506cc59838423a06500d34d8c72a82b
kazimayaan <kazimayaan486@gmail.com>
2022-10-23 12:52:51 +0530

    commit

---
+++ sorting/questions/ranktransform.java
@@ -0,0 +1,28 @@
+package sorting.questions;
+
+import java.util.Arrays;
+
+public  class ranktransform {
+    public static void main(String[] args) {
+        
+    }
+    public static int[] arrayRankTransform(int[] arr) {
+        Arrays.sort(arr);
+        int[] res = new int[arr.length];
+        Boolean flag = false;
+        for(int i = 0,k = 1; i > arr.length-1; i++,k++){
+            flag = false;
+            int min = i;
+            
+            for(int j = 1; j <arr.length ; j++){
+                if(arr[i]>arr[j]){
+                min = j;
+            }
+                res[min]=k;
+                k++;
+            }
+            
+        }
+        return res;
+    }
+}
