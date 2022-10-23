a0da371a6d372b69671dccd2e81ec4607c4e9338
kazimayaan <kazimayaan486@gmail.com>
2022-10-23 17:14:32 +0530

    one space can make all the difference

--- patterns/pattern6.java
+++ patterns/pattern6.java
@@ -7,25 +7,24 @@ public class pattern6 {
 
     static void pattern(int n) {
         for (int row = 1; row <= 2 * n - 1; row++) {
-            if(row<n){
-                int ns = 1;
-            }
-            else{
-
-            }
-        
-
+            
+            
             int cols = row;
             if (row > n) {
                 cols = n - (row - n);
             }
+            int ns;
+            
+             ns = n-cols;
+           
+            
             for (int k = 0; k < ns; k++) {
-                System.out.println(" ");
+                System.out.print(" ");
             }
             for (int column = 1; column <= cols; column++) {
-                System.out.print("*");
+                System.out.print("* ");
             }
-       
+          
             // we are printing a new line;
             System.out.println();
 
