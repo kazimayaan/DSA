c708361db8a8943293a497fd42cf93aacda7b8d5
kazimayaan <kazimayaan486@gmail.com>
2022-10-23 16:29:59 +0530

    step 1

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -9,6 +9,18 @@ public class pattern {
 
         // few steps we need to take
         // loops prerequisites 
-        
+
+        // 1, run the outer for loop the number of times you have the lines
+        // notice the rows and it becomes no of lines = no of rows = no of times outer loop will run
+// step 2
+
+
+    }
+    static void pattern(int n){
+        for (int i = 0; i < n; i++) {
+
+
+            
+        }
     }
 }
