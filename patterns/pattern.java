837717016f4fe1db07b69fcb2faac0bcc0d70c64
kazimayaan <kazimayaan486@gmail.com>
2022-10-23 16:32:15 +0530

    step 2

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -12,14 +12,16 @@ public class pattern {
 
         // 1, run the outer for loop the number of times you have the lines
         // notice the rows and it becomes no of lines = no of rows = no of times outer loop will run
-// step 2
+// step 2 how many columns are there with each row, or types of elements in the pattern
 
 
     }
     static void pattern(int n){
-        for (int i = 0; i < n; i++) {
+        for (int row = 1; row <= n; row++) {
 
-
+            for(int column = 1; column <= row; column++){
+                
+            }
             
         }
     }
