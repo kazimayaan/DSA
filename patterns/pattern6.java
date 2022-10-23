7091b7e4923c56405265a6f55eb51a9879bd8ce7
kazimayaan <kazimayaan486@gmail.com>
2022-10-23 17:08:49 +0530

    spcaes

--- patterns/pattern6.java
+++ patterns/pattern6.java
@@ -1,5 +1,34 @@
 package patterns;
 
 public class pattern6 {
-    
+    public static void main(String[] args) {
+        pattern(5);
+    }
+
+    static void pattern(int n) {
+        for (int row = 1; row <= 2 * n - 1; row++) {
+            if(row<n){
+                int ns = 1;
+            }
+            else{
+
+            }
+        
+
+            int cols = row;
+            if (row > n) {
+                cols = n - (row - n);
+            }
+            for (int k = 0; k < ns; k++) {
+                System.out.println(" ");
+            }
+            for (int column = 1; column <= cols; column++) {
+                System.out.print("*");
+            }
+       
+            // we are printing a new line;
+            System.out.println();
+
+        }
+    }
 }
