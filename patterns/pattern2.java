3a5f40a7c29dc8f624b3db3faa478fd5d6e51272
kazimayaan <kazimayaan486@gmail.com>
2022-10-23 16:37:47 +0530

    pattern2

---
+++ patterns/pattern2.java
@@ -0,0 +1,35 @@
+package patterns;
+
+
+public class pattern2{
+    public static void main(String[] args) {
+        pattern(5);
+        // learning in public
+        // patterns are important because it enhances your thoiught process
+        // pattern questions no one asks much
+        // helps imporve skills
+
+        // few steps we need to take
+        // loops prerequisites 
+
+        // 1, run the outer for loop the number of times you have the lines
+        // notice the rows and it becomes no of lines = no of rows = no of times outer loop will run
+// step 2 how many columns are there with each row, or types of elements in the pattern
+// step three check what do yoy need to print
+// only three steps
+
+
+    }
+    static void pattern(int n){
+        for (int row = 1; row <= n; row++) {
+
+            for(int column = n; column >= row; column--){
+                System.out.print("*");
+            }
+            // we are printing a new line;
+            System.out.println();
+            
+        }
+   
+
+    }}
