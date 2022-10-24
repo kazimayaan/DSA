3fcf4d96e6402cf704946369c3def3d6af7386be
kazimayaan <kazimayaan486@gmail.com>
2022-10-24 19:07:15 +0530

    strings

---
+++ strings/leeycode.java
@@ -0,0 +1,20 @@
+package strings;
+
+import java.util.Arrays;
+
+public class leeycode {
+    public static void main(String[] args) {
+        String s = "Hello How are you";
+        String[] ll = s.split(" ");
+        System.out.println(Arrays.toString(ll));
+
+    }
+    private static String lol(String s)
+    {
+        String[] lol = s.split(" ");
+        System.out.println(Arrays.toString(lol));
+
+        String ss = "s";
+        return ss;
+    }
+}
