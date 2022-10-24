9552ac329c68ad1ff2df32b26dd4210593439907
kazimayaan <kazimayaan486@gmail.com>
2022-10-24 16:55:31 +0530

    fixed

--- recursion/binarysearch.java
+++ recursion/binarysearch.java
@@ -3,14 +3,20 @@ package recursion;
 public class binarysearch {
     public static void main(String[] args) {
         int[] arr = {1,2,4,5,6,77,88};
-        int target = 1;
+        int target = 10;
         int index = binarysearch(arr,target, 0 , arr.length-1);
+        if(index==-1){
+            System.out.println("not found sorry, enter again");
+        }
         System.out.println(index);
     }
 
     private static int binarysearch(int[] arr, int target, int i, int j) {
         int e = j;
         int s = i;
+        if(s>e){
+            return -1;
+        }
         int mid = s + (e-s)/2;
 
         if(arr[mid]==target){
