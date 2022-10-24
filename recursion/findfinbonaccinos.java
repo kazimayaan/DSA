968e51819fcc55a7c1cea36ee95494cb32ae58ec
kazimayaan <kazimayaan486@gmail.com>
2022-10-24 16:09:11 +0530

    trying

--- recursion/findfinbonaccinos.java
+++ recursion/findfinbonaccinos.java
@@ -9,14 +9,14 @@ public class findfinbonaccinos {
 int n = 0;
 if(i == 1 || i == 0){
     return i;
-    
+
 }
-        while(i>1){
+        
 
         
         n= fibonacci(i-1)-fibonacci(i-2);
 
-    }
+    
         return n;
     }
 }
