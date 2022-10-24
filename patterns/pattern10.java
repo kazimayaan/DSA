b4cb96ec77ada8e00d997b99df34ac462a892abe
kazimayaan <kazimayaan486@gmail.com>
2022-10-24 14:16:45 +0530

    making changes

--- patterns/pattern10.java
+++ patterns/pattern10.java
@@ -25,7 +25,7 @@ int digit =2 * n-1;
     }
     static void pattern31(int n) {
         int originalN = n;
-        n = 2 * n;
+        n = 2 * n-1;
         for (int row = 0; row <= n; row++) {
             for (int col = 0; col <= n; col++) {
                 int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
