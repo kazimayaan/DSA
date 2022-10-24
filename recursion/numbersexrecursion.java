5869fd1217b82743f9b99a122df6dba478d9280e
kazimayaan <kazimayaan486@gmail.com>
2022-10-24 15:05:31 +0530

    issues

--- recursion/numbersexrecursion.java
+++ recursion/numbersexrecursion.java
@@ -2,9 +2,21 @@ package recursion;
 
 public class numbersexrecursion {
     public static void main(String[] args) {
-       print(3) ;
+    //    print(3) ;
+       prinet(1);
+    }
+    private static void prinet(int i) {
+        if(i == 5){
+            System.out.println(i);
+            return;
+        }
+        prinet(i);
+        i++;
+
     }
     private static void print(int i) {
+
+
  
 System.out.println(i);
 i++;
