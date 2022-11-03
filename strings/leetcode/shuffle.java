e2da5f5b62b21814e999d255fa72394334210149
kazimayaan <kazimayaan486@gmail.com>
2022-11-03 13:06:42 +0530

    prettier

--- strings/leetcode/shuffle.java
+++ strings/leetcode/shuffle.java
@@ -5,22 +5,21 @@ import java.util.Arrays;
 public class shuffle {
     public static void main(String[] args) {
         String s = "codeleet";
-        int[] indices = {4,5,6,7,0,2,1,3};
-      String res =   restoreString(s,indices);
+        int[] indices = { 4, 5, 6, 7, 0, 2, 1, 3 };
+        String res = restoreString(s, indices);
         System.out.println(res);
     }
 
-    
     public static String restoreString(String s, int[] indices) {
         StringBuilder res = new StringBuilder();
         res.setLength(indices.length);
         int i = 0;
-         while(i< indices.length){
-             char f = s.charAt(i);
-             int index = indices[i];
-             res.insert(index,f);
-             i++;
-         }
+        while (i < indices.length) {
+            char f = s.charAt(i);
+            int index = indices[i];
+            res.insert(index, f);
+            i++;
+        }
         return res.toString();
     }
 }
