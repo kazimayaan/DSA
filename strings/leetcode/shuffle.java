19d91eb4a5c64a264079de68d43378ed7c8720b2
kazimayaan <kazimayaan486@gmail.com>
2022-11-03 13:01:02 +0530

    func

--- strings/leetcode/shuffle.java
+++ strings/leetcode/shuffle.java
@@ -4,6 +4,18 @@ public class shuffle {
     public static void main(String[] args) {
         String s = "codeleet";
         int[] indices = new int[s.length()];
-        
+
+    }
+    public String restoreString(String s, int[] indices) {
+        StringBuilder res = new StringBuilder();
+        res.setLength(indices.length);
+        int i = 0;
+         while(i< indices.length){
+             char f = s.charAt(i);
+             int index = indices[i];
+             res.insert(index,f);
+             i++;
+         }
+        return res.toString();
     }
 }
