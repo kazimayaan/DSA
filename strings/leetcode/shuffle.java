e645dcad19d7ceda4df14cd1826f4a82ddc72489
kazimayaan <kazimayaan486@gmail.com>
2022-11-03 13:00:56 +0530

    commit

--- strings/leetcode/shuffle.java
+++ strings/leetcode/shuffle.java
@@ -3,7 +3,7 @@ package strings.leetcode;
 public class shuffle {
     public static void main(String[] args) {
         String s = "codeleet";
+        int[] indices = new int[s.length()];
         
-
     }
 }
