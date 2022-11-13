352a15693998bf8f2d3109f03328b7d134e0855e
kazimayaan <kazimayaan486@gmail.com>
2022-11-14 00:46:51 +0530

    bit

---
+++ numbertheory/arrduplicate.java
@@ -0,0 +1,13 @@
+package numbertheory;
+
+public class arrduplicate {
+    public static void main(String[] args) {
+        int[] arr = {1,2,1,2,3,4,4};
+        System.out.println(dupl(arr));
+    }
+
+    private static int dupl(int[] arr) {
+
+        
+    }
+}
