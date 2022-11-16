73c749c27d5cde26f696ca8d4babc5688f860cab
kazimayaan <kazimayaan486@gmail.com>
2022-11-16 19:56:15 +0530

    amns

--- numbertheory/rangexor.java
+++ numbertheory/rangexor.java
@@ -2,7 +2,12 @@ package numbertheory;
 
 public class rangexor {
     public static void main(String[] args) {
-        
+        int a = 3;
+        int b = 9;
+
+        int mid = xor(b);
+        int ans = mid ^ xor(a-1);
+        System.out.println(ans);
     }
     private static int xor(int n) {
 
