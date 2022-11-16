62f39bfddad2a2c84cff9fdd077de941b09083d0
kazimayaan <kazimayaan486@gmail.com>
2022-11-16 19:38:57 +0530

    my code

---
+++ numbertheory/setbits.java
@@ -0,0 +1,15 @@
+package numbertheory;
+
+public class setbits {
+    public static void main(String[] args) {
+        int n = 9;
+        int ans = 0;
+        while(n>0){
+            if((n&1)==1){
+                ans++;
+            }
+            n= n>>1;
+        }
+        System.out.println(ans);
+    }
+}
