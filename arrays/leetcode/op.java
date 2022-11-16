a105871bb3abe4521f13b1659c46016ff4c28bbf
kazimayaan <kazimayaan486@gmail.com>
2022-11-16 11:55:02 +0530

    op

---
+++ arrays/leetcode/op.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class op {
+    public static void main(String[] args) {
+        int[] nums = {1,2,0,4,0,0,3};
+        int[] arr= applyOperations(nums);
+        System.out.println(Arrays.toString(arr));
+
+        
+    }
+    public static int[] applyOperations(int[] nums) {
+        for(int i =0; i < nums.length-1; i++){
+           if (nums[i]==nums[i+1]){
+               nums[i]=nums[i]*2;
+               nums[i+1]=0;
+           }
+        }
+        int z = 0;
+        
+        for (int i =0; i< nums.length;i++){
+            if(nums[i]!=0){
+                nums[i]=nums[z];
+                z++;
+            }
+        }
+        return nums;
+    }
+}
