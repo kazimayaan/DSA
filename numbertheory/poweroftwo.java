dd4253975ece816b43756328d7645abe8720cd1e
kazimayaan <kazimayaan486@gmail.com>
2022-11-16 19:29:47 +0530

    numt

---
+++ numbertheory/poweroftwo.java
@@ -0,0 +1,11 @@
+package numbertheory;
+
+public class poweroftwo {
+    public static void main(String[] args) {
+        int n = 0;
+        if(n == 0){
+            System.out.println(false);
+
+        }
+    }
+}
