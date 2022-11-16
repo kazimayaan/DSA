1049f53af4df1771f50b7b6c564c51759970d864
kazimayaan <kazimayaan486@gmail.com>
2022-11-16 14:23:43 +0530

    log

---
+++ numbertheory/knownumberofdigitsinbinary.java
@@ -0,0 +1,12 @@
+package numbertheory;
+
+public class knownumberofdigitsinbinary {
+    public static void main(String[] args) {
+        int n = 342432;
+        int base = 10;
+
+        int ans = (int)(Math.log(n)/Math.log(base))+1;
+
+
+    }
+}
