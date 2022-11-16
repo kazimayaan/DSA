919db391daebe41ffaa08cae97f8c67edf2643e5
kazimayaan <kazimayaan486@gmail.com>
2022-11-16 14:25:12 +0530

    s

--- numbertheory/knownumberofdigitsinbinary.java
+++ numbertheory/knownumberofdigitsinbinary.java
@@ -10,7 +10,9 @@ public class knownumberofdigitsinbinary {
         System.out.println(ans);
         int counter=0;
         while(n>0){
-
+            counter++;
+            n= n>>1;
         }
+        System.out.println(counter);
     }
 }
