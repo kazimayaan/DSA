8cdc4908b9cac277a0662827af49cbb3f7daa58e
kazimayaan <kazimayaan486@gmail.com>
2022-11-16 19:30:36 +0530

    sol

--- numbertheory/poweroftwo.java
+++ numbertheory/poweroftwo.java
@@ -2,7 +2,7 @@ package numbertheory;
 
 public class poweroftwo {
     public static void main(String[] args) {
-        int n = 0;
+        int n = 566;
         if(n == 0){
             System.out.println(false);
 
