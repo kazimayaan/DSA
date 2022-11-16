b8b75624010140c12ab47c6c2d45a16f47d0bebe
kazimayaan <kazimayaan486@gmail.com>
2022-11-16 14:25:56 +0530

    comment

--- numbertheory/knownumberofdigitsinbinary.java
+++ numbertheory/knownumberofdigitsinbinary.java
@@ -3,9 +3,9 @@ package numbertheory;
 public class knownumberofdigitsinbinary {
     public static void main(String[] args) {
         int n = 342432;
-        int base = 2;
+        int base = 16;
 
-        
+
         int ans = (int)(Math.log(n)/Math.log(base))+1;
 
         System.out.println(ans);
@@ -16,4 +16,5 @@ public class knownumberofdigitsinbinary {
         }
         System.out.println(counter);
     }
+    // complexity is logn
 }
