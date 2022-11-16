1c570b4d4ab6921a349d769f2ab8291f4f8d70f9
kazimayaan <kazimayaan486@gmail.com>
2022-11-16 19:30:56 +0530

    true

--- numbertheory/poweroftwo.java
+++ numbertheory/poweroftwo.java
@@ -2,7 +2,7 @@ package numbertheory;
 
 public class poweroftwo {
     public static void main(String[] args) {
-        int n = 566;
+        int n = 8;
         if(n == 0){
             System.out.println(false);
 
