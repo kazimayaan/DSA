6a450c9fc99c3ebe3f57330e090534e7a2d57d8e
kazimayaan <kazimayaan486@gmail.com>
2022-11-20 19:09:03 +0530

    optimised solution

--- numbertheory/factorsoptimised.java
+++ numbertheory/factorsoptimised.java
@@ -1,5 +1,16 @@
 package numbertheory;
 
 public class factorsoptimised {
-    
+    public static void main(String[] args) {
+        factors(20);
+    } 
+    public static void factors(int n ){
+        for(int i = 1; i*i <= n; i++){
+            if(n%i==0){
+                int x = n/i;
+
+                System.out.print(i+ " " + x+" ");
+            }
+        }
+    }
 }
