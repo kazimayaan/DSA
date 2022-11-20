6a47f25598376b951bd18af64b17e975d983d659
kazimayaan <kazimayaan486@gmail.com>
2022-11-20 19:04:18 +0530

    workidn

--- numbertheory/factors.java
+++ numbertheory/factors.java
@@ -3,7 +3,7 @@ package numbertheory;
 public class factors {
     public static void main(String[] args) {
         factors(50);
-    }
+    } 
     public static void factors(int n ){
         for(int i = 1; i <= n; i++){
             if(n%i==0){
