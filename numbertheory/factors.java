bf87553f37d22c280fcce5c02c62f9c71dc2b3a3
kazimayaan <kazimayaan486@gmail.com>
2022-11-20 19:04:44 +0530

    repeating

--- numbertheory/factors.java
+++ numbertheory/factors.java
@@ -2,7 +2,7 @@ package numbertheory;
 
 public class factors {
     public static void main(String[] args) {
-        factors(50);
+        factors(20);
     } 
     public static void factors(int n ){
         for(int i = 1; i <= n; i++){
