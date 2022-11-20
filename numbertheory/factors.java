7e5a80faba3fcbef389d4f8741a91c8356fdfb27
kazimayaan <kazimayaan486@gmail.com>
2022-11-20 19:02:14 +0530

    factorsn ofn n

---
+++ numbertheory/factors.java
@@ -0,0 +1,10 @@
+package numbertheory;
+
+public class factors {
+    public static void main(String[] args) {
+        factors(50);
+    }
+    public static void factors(int n ){
+        
+    }
+}
