99b9b8e3801702d188cf4248d601153108e10d55
kazimayaan <kazimayaan486@gmail.com>
2022-11-20 17:29:42 +0530

    lol

--- numbertheory/sqrt.java
+++ numbertheory/sqrt.java
@@ -4,4 +4,27 @@ public class sqrt {
     public static void main(String[] args) {
         // well use binary search
     }
+    public int mySqrt(int x) {
+        if(x == 1){
+            return 1;
+        }
+        long ans=0;
+        long s = 0;
+        long e = x;
+        while(s<=e){
+            long mid = s+(e-s)/2;
+            if(mid *mid == x){
+                return (int)mid;
+            }
+            else if (mid*mid < x){
+                s = mid+1;
+                ans=mid;
+            }
+            else if (mid * mid > x){
+                e = mid-1;
+            }
+            
+        }
+        return (int)ans;
+    }
 }
