1ae3bdb95a3585742997bc8816d4ce4d38d290be
kazimayaan <kazimayaan486@gmail.com>
2022-11-20 19:43:52 +0530

    corr

--- numbertheory/gcd.java
+++ numbertheory/gcd.java
@@ -2,7 +2,7 @@ package numbertheory;
 
 public class gcd {
     public static void main(String[] args) {
-        System.out.println(euclidiangcd(4, 9));
+        System.out.println(euclidiangcd(3, 7));
         ;
     }
 
