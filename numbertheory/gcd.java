a21f602bbd56e6cad304af8e104cb49666cdbcaf
kazimayaan <kazimayaan486@gmail.com>
2022-11-20 19:42:10 +0530

    proper

--- numbertheory/gcd.java
+++ numbertheory/gcd.java
@@ -2,17 +2,17 @@ package numbertheory;
 
 public class gcd {
     public static void main(String[] args) {
-       System.out.println(euclidiangcd(4, 9)); ;
+        System.out.println(euclidiangcd(4, 9));
+        ;
     }
 
+    public static int euclidiangcd(int a, int b) {
 
-    public static int euclidiangcd(int a,int b){
-
-        if(a==0){
+        if (a == 0) {
             return b;
         }
 
-        return euclidiangcd(b%a,a);
-        
+        return euclidiangcd(b % a, a);
+
     }
 }
