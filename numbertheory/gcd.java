e38a8c0a6a4b2d51fbca785deae5a6b9292dc19c
kazimayaan <kazimayaan486@gmail.com>
2022-11-20 19:38:36 +0530

    gcd

---
+++ numbertheory/gcd.java
@@ -0,0 +1,14 @@
+package numbertheory;
+
+public class gcd {
+    public static void main(String[] args) {
+        
+    }
+
+
+    public static int euclidiangcd(int a,int b){
+        
+        return gcd(b%a,a)
+        
+    }
+}
