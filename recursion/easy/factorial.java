01967348279a6a33036194f0f128d88ab519bfaa
kazimayaan <kazimayaan486@gmail.com>
2022-11-21 19:20:30 +0530

    fACTORIAL RECURSION

--- recursion/easy/factorial.java
+++ recursion/easy/factorial.java
@@ -2,6 +2,14 @@ package recursion.easy;
 
 public class factorial {
     public static void main(String[] args) {
-        
+        System.out.println(fact(5));
+    
+    }
+
+    private static int fact(int i) {
+        if(i==1){
+            return 1;
+        }
+        return i*fact(i-1);
     }
 }
