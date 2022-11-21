d99c84b72b3e2d1c124023c27d211399eb3566e6
kazimayaan <kazimayaan486@gmail.com>
2022-11-21 18:53:50 +0530

    true

--- recursion/armstr.java
+++ recursion/armstr.java
@@ -6,7 +6,19 @@ public class armstr {
         System.out.println(isarm(n));
     }
 
-    private static char[] isarm(int n) {
-        return null;
+    private static boolean isarm(int n) {
+        int x = n;
+        int ans = 0;
+        while(x>0){
+            int y = x%10;
+            ans = ans + (y*y*y);
+            x = x/10;
+        }
+        System.out.println(ans);
+        if(ans==n){
+            return true;
+
+        }
+        return false;
     }
 }
