8d89f48f9146954fb4c12d1152c47aef5af20a13
kazimayaan <kazimayaan486@gmail.com>
2022-11-21 19:11:45 +0530

    cka

--- recursion/easy/printn.java
+++ recursion/easy/printn.java
@@ -2,16 +2,27 @@ package recursion.easy;
 
 public class printn {
     public static void main(String[] args) {
-        print(10);
+        printrev(10);
+        print(5);
     }
 
-    private static void print(int i) {
+    private static void printrev(int i) {
         if(i ==0){
             return;
         }
         // soprint(i);
+        printrev(i-1);
+        System.out.print(i+ " ");
+
+    }
+    private static void print(int i) {
+        if(i ==0){
+            return;
+        }
+        
+        // soprint(i);
+        System.out.print(i+ " ");
         print(i-1);
-        System.out.println(i);
 
     }
 }
