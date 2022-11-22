7d6baacea6ece6f5f1dcb778048d14d20b4f38c3
kazimayaan <kazimayaan486@gmail.com>
2022-11-22 14:59:16 +0530

    concept

--- recursion/easy/digitproduct.java
+++ recursion/easy/digitproduct.java
@@ -2,14 +2,13 @@ package recursion.easy;
 
 public class digitproduct {
     public static void main(String[] args) {
-        int n = 1234;
+        int n = 12347;
         int pro = digitproduct(n);
         System.out.println(pro);
     }
 
     private static int digitproduct(int n) {
         if(n==0){
-
             return 1;
         }
         return (n%10) * digitproduct(n/10);
