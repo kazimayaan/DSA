da5ce05b5a5bbc138fc7beb6ec3558f17ef5eaf7
kazimayaan <kazimayaan486@gmail.com>
2022-11-23 08:45:49 +0530

    a

--- numbertheory/powerbinary.java
+++ numbertheory/powerbinary.java
@@ -6,6 +6,17 @@ public class powerbinary {
         int power = 6;
 
         int ans = 1;
+        int k = 111010;
+        int count = 0;
+        for(int i = 0; i< 5; i++){
+            // System.out.println(k);
+            if((k&1) == 1){
+                count++;
+            }
+            k = k>>1;
+            
+        }
+        System.out.println(count);
         while(power>0){
             if((power&1)==1){
                 ans*= base;
