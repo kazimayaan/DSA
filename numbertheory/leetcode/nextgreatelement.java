f5f686e8ff4b1fc1cccab0d2089900892ec7e30b
kazimayaan <kazimayaan486@gmail.com>
2022-11-23 09:05:51 +0530

    func

--- numbertheory/leetcode/nextgreatelement.java
+++ numbertheory/leetcode/nextgreatelement.java
@@ -2,6 +2,35 @@ package numbertheory.leetcode;
 
 public class nextgreatelement {
     public static void main(String[] args) {
-        
+        int n = 1234;
+
+    }
+    public int nextGreaterElement(int n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();  
+        int x = n;
+        int digits = 0;
+        while(x>0){
+            list.add(x%10);
+            x/=10;
+            digits++;
+        }
+       
+        for ( int i = list.size()-1; i> 0 ; i--){
+            if(list.get(i)>list.get(i-1)){
+                 Collections.swap(list, i, i-1);
+                break;
+            }
+}
+        int ans = 0;
+        for(int nos: list)  
+                    {  
+                    ans = ans *10 + nos;
+                    }  
+        if(ans==n){
+            return -1;
+        }
+        else{
+            return ans;
+        }
     }
 }
