26a501b0a471715e5cb771596d287316a8126420
kazimayaan <kazimayaan486@gmail.com>
2022-11-23 09:17:20 +0530

    correcting

--- numbertheory/leetcode/nextgreatelement.java
+++ numbertheory/leetcode/nextgreatelement.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 
 public class nextgreatelement {
     public static void main(String[] args) {
-        int n = 1234;
+        int n =230241;
         System.out.println(nextGreaterElement(n));
 
     }
@@ -18,11 +18,14 @@ public class nextgreatelement {
             x/=10;
             digits++;
         }
+        int minin = list.get(0);
        
         for ( int i = 0; i<list.size()-1 ; i++){
             int y = list.get(i);
             int z = list.get(i+1);
-
+            if(y<z){
+                minin = Math.min(minin,y);
+            }
             if(list.get(i)>list.get(i+1)){
                  Collections.swap(list, i, i+1);
                 break;
