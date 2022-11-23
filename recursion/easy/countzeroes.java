c20cbed131f1343b663d00d3dae5fef6e33af965
kazimayaan <kazimayaan486@gmail.com>
2022-11-23 11:20:46 +0530

    recursion countzeros

---
+++ recursion/easy/countzeroes.java
@@ -0,0 +1,23 @@
+package recursion.easy;
+
+public class countzeroes {
+    public static void main(String[] args) {
+        int n = 100101011;
+        System.out.println(cz(n));
+    }
+
+    private static int cz(int n) {
+      int sol = count(n,0);
+      return sol;
+    }
+
+    private static int count(int n, int i) {
+        if(n==0){
+            return i;
+        }
+        if(n%10==0){
+            i++;
+        }
+        return count(n/10,i);
+    }
+}
