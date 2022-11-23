13fcbe5deb7f24a5e7bcb6f3f7d28cdc6aa8e81b
kazimayaan <kazimayaan486@gmail.com>
2022-11-23 10:49:06 +0530

    checking test cases

--- recursion/easy/palindrome.java
+++ recursion/easy/palindrome.java
@@ -2,21 +2,25 @@ package recursion.easy;
 
 public class palindrome {
     public static void main(String[] args) {
-        int n = 1234321;
+        int n = 121;
         System.out.println(ispali(n));
     }
 
 private static boolean ispali(int n) {
-      int m = rev(n);
+      int m = rev(n,0);
+      System.out.println(m);
       if(m==n){
         return true;
       }
       return false;
     }
 
-private static int rev(int n) {
-    
-    return 0;
+private static int rev(int n, int ans) {
+    if(n==0){
+        return ans;
+    }
+    ans = ans*10 + (n%10);
+    return rev(n/10,ans);
 }
 
 }
