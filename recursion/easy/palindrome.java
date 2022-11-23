7c84bfcd1e31cceb8346ae2d42070d3f951b50f1
kazimayaan <kazimayaan486@gmail.com>
2022-11-23 10:49:34 +0530

    correct ciode

--- recursion/easy/palindrome.java
+++ recursion/easy/palindrome.java
@@ -2,13 +2,12 @@ package recursion.easy;
 
 public class palindrome {
     public static void main(String[] args) {
-        int n = 121;
+        int n = 1234321;
         System.out.println(ispali(n));
     }
 
 private static boolean ispali(int n) {
       int m = rev(n,0);
-      System.out.println(m);
       if(m==n){
         return true;
       }
