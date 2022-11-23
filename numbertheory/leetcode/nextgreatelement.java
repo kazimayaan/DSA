bd39cf10347201a34065e6416ededb44627e4573
kazimayaan <kazimayaan486@gmail.com>
2022-11-23 09:06:22 +0530

    print

--- numbertheory/leetcode/nextgreatelement.java
+++ numbertheory/leetcode/nextgreatelement.java
@@ -3,6 +3,7 @@ package numbertheory.leetcode;
 public class nextgreatelement {
     public static void main(String[] args) {
         int n = 1234;
+        System.out.println(nextGreaterElement(n));
 
     }
     public static int nextGreaterElement(int n) {
