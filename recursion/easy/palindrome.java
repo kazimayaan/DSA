ac8ba813ce0f92619c52db7544c4496e3fbe2425
kazimayaan <kazimayaan486@gmail.com>
2022-11-23 10:44:00 +0530

    method

--- recursion/easy/palindrome.java
+++ recursion/easy/palindrome.java
@@ -3,5 +3,11 @@ package recursion.easy;
 public class palindrome {
     public static void main(String[] args) {
         int n = 1234321;
+        System.out.println(ispali(n));
     }
+
+    private static char[] ispali(int n) {
+        return null;
+    }
+
 }
