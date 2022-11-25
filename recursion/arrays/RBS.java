ab97b92b4fd99a2cf62d2606ae5737d468e3cc4e
kazimayaan <kazimayaan486@gmail.com>
2022-11-25 13:11:27 +0530

    rbs

---
+++ recursion/arrays/RBS.java
@@ -0,0 +1,14 @@
+package recursion.arrays;
+
+public class RBS {
+    public static void main(String[] args) {
+        int[] arr= {5,6,7,8,1,2,3,4};
+        int target = 6;
+        int index = rbs(arr,target);
+        System.out.println(index);
+    }
+
+    private static int rbs(int[] arr, int target) {
+        return 0;
+    }
+}
