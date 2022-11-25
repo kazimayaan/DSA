6208a82709015b199c49929d7a69d65634a3258c
kazimayaan <kazimayaan486@gmail.com>
2022-11-25 13:25:05 +0530

    a

--- recursion/arrays/RBS.java
+++ recursion/arrays/RBS.java
@@ -24,14 +24,14 @@ public class RBS {
                     s = mid+1;
                 }
             }
-            if(arr[mid]>=arr[s]){
-                if(target>=arr[s] && target<= arr[mid]){
-                    e = mid -1;
-                }
-                else{
+            
+                if(target>=arr[mid] && target<= arr[e]){
                     s = mid+1;
                 }
-            }
+                else{
+                    e = mid -1;
+                }
+            
         }
         return 0;
     }
