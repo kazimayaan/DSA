76877738a22505f4f5a608ba4e3c7b6fc975c9bf
kazimayaan <kazimayaan486@gmail.com>
2022-11-25 12:38:08 +0530

    a

--- recursion/arrays/lsearch.java
+++ recursion/arrays/lsearch.java
@@ -16,11 +16,11 @@ public class lsearch {
     }
 
     private static ArrayList<Integer> lists1(int[] arr, int target, int i, ArrayList<Integer> lists,) {
-        if(index==arr.length-1){
+        if(i==arr.length-1){
             return lists;
         }
         if(arr[i]==target){
-            list.add(index);
+            list.add(i);
         }
     }
 
