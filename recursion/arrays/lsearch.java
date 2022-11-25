fbdab2fce44880d89a1af6cae7a584e6aae0a19f
kazimayaan <kazimayaan486@gmail.com>
2022-11-25 12:36:48 +0530

    m

--- recursion/arrays/lsearch.java
+++ recursion/arrays/lsearch.java
@@ -2,17 +2,19 @@ package recursion.arrays;
 
 import java.util.ArrayList;
 
+import sorting.questions.arithmaticptogressuibs;
+
 public class lsearch {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,8,9,8};
         int target = 8;
         ArrayList<Integer> list = lists(arr,target,0);
         System.out.println(list);
-        ArrayList<Integer> lists= lists1(arr,target,0);
+        ArrayList<Integer> lists= new ArrayList<>()
         System.out.println(lists);
     }
 
-    private static ArrayList<Integer> lists1(int[] arr, int target, int i) {
+    private static ArrayList<Integer> lists1(int[] arr, int target, int i,) {
         return null;
     }
 
