76489c0b106fb94549443f9e86c643e6e7587ff8
kazimayaan <kazimayaan486@gmail.com>
2022-11-25 13:12:58 +0530

    mid

--- recursion/arrays/RBS.java
+++ recursion/arrays/RBS.java
@@ -12,6 +12,11 @@ public class RBS {
         int s = 0;
         int e = arr.length-1;
         int mid = s+(e-s)/2;
+        while(s<=e){
+            if(arr[mid]==target){
+                return mid;
+            }
+        }
         return 0;
     }
 }
