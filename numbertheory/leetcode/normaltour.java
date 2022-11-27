09b2b69468fbec1074eea7e401d57337a942a2af
kazimayaan <kazimayaan486@gmail.com>
2022-11-27 18:43:20 +0530

    cpcv

--- numbertheory/leetcode/normaltour.java
+++ numbertheory/leetcode/normaltour.java
@@ -4,4 +4,19 @@ public class normaltour {
     public static void main(String[] args) {
         int n = 14;
     }
+    static int m = 0;
+    public int numberOfMatches(int n) {
+        if(n==2){
+            return m +1;
+        }
+        if(n<=1){
+            return 0;
+        }
+        if(n%2==0){
+            m = m + n/2;
+           return numberOfMatches(n/2);
+        }
+        m = m +n/2;
+     return    numberOfMatches(n/2 +1);
+    }    
 }
