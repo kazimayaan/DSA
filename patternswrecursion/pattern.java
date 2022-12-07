f9eb2b19d1ff56581f604c0b23db617308a52d63
kazimayaan <kazimayaan486@gmail.com>
2022-12-07 17:32:53 +0530

    myself i did it

--- patternswrecursion/pattern.java
+++ patternswrecursion/pattern.java
@@ -2,6 +2,20 @@ package patternswrecursion;
 
 public class pattern {
     public static void main(String[] args) {
-        
+        pattern(4,4);
+    }
+    public static void pattern(int row, int col){
+            if(row==0){
+                return;
+            }
+            int f = col;
+            System.out.println();
+            if(row>0 && col>0){
+                while(f>0){
+                    System.out.print("*");
+                    f--;
+                }
+            }
+            pattern(--row,--col);
     }
 }
