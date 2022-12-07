93e5a4308de240a8c837bd1fc262b19fa4d73dac
kazimayaan <kazimayaan486@gmail.com>
2022-12-07 18:31:43 +0530

    q

--- patternswrecursion/selection.java
+++ patternswrecursion/selection.java
@@ -3,6 +3,10 @@ package patternswrecursion;
 public class selection {
     public static void main(String[] args) {
         int[] arr = {12,11,13,17,99,100};
-        
+        selection(arr, arr.length-1, 0, 0);
+    }
+
+    private static void selection(int[] arr, int i, int j, int k) {
+        qq
     }
 }
