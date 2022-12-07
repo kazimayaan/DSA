5ba30a17d51c2b9904a6198b35b41723985e3a43
kazimayaan <kazimayaan486@gmail.com>
2022-12-07 18:49:30 +0530

    finally

--- patternswrecursion/selection.java
+++ patternswrecursion/selection.java
@@ -1,18 +1,35 @@
 package patternswrecursion;
 
+import java.util.Arrays;
+
 public class selection {
     public static void main(String[] args) {
         int[] arr = {12,11,13,17,99,100};
-        selection(arr, arr.length-1, 0, 0);
+        selection(arr, arr.length, 0, 0);
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void selection(int[] arr, int i, int j, int k) {
       if(i == 0){
         return;
       }
-      if(arr[k]<arr[k+1]){
-        k = k+1; 
+      if(j < i){
+
+      
+      if(arr[j]>arr[k]){
+          selection(arr, i, j + 1, j);
+          
+        }
+        else{
+          selection(arr, i, j+1, k);
+
       }
+    }
+    else{
+        swap(k, i-1, arr);
+        selection(arr, --i, 0, 0);
+    }
+
     }
     private static void swap(int a, int b, int[] arr){
         int temp = arr[a];
