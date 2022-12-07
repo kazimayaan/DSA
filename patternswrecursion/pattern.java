de46a4b210531b94007d42fbd4909aa7081bc1ea
kazimayaan <kazimayaan486@gmail.com>
2022-12-07 17:34:08 +0530

    yes

--- patternswrecursion/pattern.java
+++ patternswrecursion/pattern.java
@@ -3,6 +3,7 @@ package patternswrecursion;
 public class pattern {
     public static void main(String[] args) {
         pattern(4,4);
+
     }
     public static void pattern(int row, int col){
             if(row==0){
