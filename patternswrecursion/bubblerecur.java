99d4b082840cde8a79631d69c1a1e98e560bc252
kazimayaan <kazimayaan486@gmail.com>
2022-12-07 18:17:52 +0530

    bubble

--- patternswrecursion/bubblerecur.java
+++ patternswrecursion/bubblerecur.java
@@ -5,10 +5,30 @@ import java.util.Arrays;
 public class bubblerecur {
     public static void main(String[] args) {
         int[] arr = {2,3,1,8,7,6,4,5};
-        bubble(arr);
+        bubble(arr, arr.length-1, 0);
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void bubble(int[] arr) {
+    private static void bubble(int[] arr, int len, int ind) {
+        if(len==0){
+            return;
+        }
+      if(ind<len){
+
+        if(arr[ind]>arr[ind+1]){
+            swap(ind,ind+1,arr);
+
+        }
+        bubble(arr, len, ind+1);
+    }
+        else{
+            bubble(arr, --len, 0);
+        }
+    }
+
+    private static void swap(int a, int b, int[] arr){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b]= temp;
     }
 }
