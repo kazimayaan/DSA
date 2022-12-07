45617868a183f76455d8be360b39588199728952
kazimayaan <kazimayaan486@gmail.com>
2022-12-07 18:09:46 +0530

    a

--- patternswrecursion/bubblerecur.java
+++ patternswrecursion/bubblerecur.java
@@ -1,7 +1,14 @@
 package patternswrecursion;
 
+import java.util.Arrays;
+
 public class bubblerecur {
     public static void main(String[] args) {
-        
+        int[] arr = {2,3,1,8,7,6,4,5};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubble(int[] arr) {
     }
 }
