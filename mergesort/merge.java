d59677814e7771e602936079358a2694308e7df4
kazimayaan <kazimayaan486@gmail.com>
2022-12-08 17:47:35 +0530

    a

--- mergesort/merge.java
+++ mergesort/merge.java
@@ -19,21 +19,32 @@ public class merge {
         return merge(left,right);
     }
     private static int[] merge(int[] left, int[] right) {
-        int[] mix = new int[left.length+right.length ];
+        int[] mix = new int[left.length+right.length];
         int i = 0; 
         int j = 0; 
         int k = 0;
         while(i<left.length && j < right.length){
             if(left[i]<right[j]){
                 mix[k]=left[i];
-                k++;
+             
                 i++;
             }
             if(right[j]<left[i]){
                 mix[k]=right[j];
                 j++;
-                k++;
             }
+            k++;
+
+        }
+        while(i<left.length){
+            mix[k]=left[i];
+            i++;
+            k++;
+        }
+        while(j<right.length){
+            mix[k]=right[j];
+            j++;
+            k++;
         }
         return mix;
     }
