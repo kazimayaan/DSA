7fef5a9ba9c81732c2168c3eae727ad57bb497f7
kazimayaan <kazimayaan486@gmail.com>
2022-12-08 17:42:45 +0530

    lol

--- mergesort/merge.java
+++ mergesort/merge.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class merge {
     public static void main(String[] args) {
         int[] arr = {1,6,3,4,2,9,19};
-        int[] lol = mergesort(arr)
+        int[] lol = mergesort(arr);
+        System.out.println();
 
     }
     static int[] mergesort(int[] arr){
