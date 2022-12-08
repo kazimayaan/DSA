1953a330d39f84f2a29e6e13c4737c309aa456ad
kazimayaan <kazimayaan486@gmail.com>
2022-12-08 12:45:55 +0530

    s

--- recursionarray/checkifsorted.java
+++ recursionarray/checkifsorted.java
@@ -2,18 +2,18 @@ package recursionarray;
 
 public class checkifsorted {
     public static void main(String[] args) {
-        int[] arr = { 1,2,3,4,5,0,8,9};
-        System.out.println(sorted(arr,0,arr.length-1));
+        int[] arr = { 1,2,3,4,5,7,8,9};
+        System.out.println(sorted(arr,0));
     }
 
-    private static boolean sorted(int[] arr, int ind, int last) {
-        if(ind==last){
+    private static boolean sorted(int[] arr, int ind) {
+        if(ind==arr.length-1){
 
             return true ;
         }
         if(arr[ind]>arr[ind+1]){
             return false;
         }
-        return sorted(arr, ++ind, last);
+        return sorted(arr, ++ind);
     }
 }
