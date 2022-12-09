2198a65ef3f9dbc7452d2fe055e49d2938641cff
kazimayaan <kazimayaan486@gmail.com>
2022-12-09 20:39:38 +0530

    z

---
+++ mergesort/mergfesort.java
@@ -0,0 +1,16 @@
+package mergesort;
+
+import java.util.Arrays;
+
+public class mergfesort {
+    public static void main(String[] args) {
+        int[] arr = {1,6,3,4,2,9,19};
+        int[] lol = mergesort(arr);
+        System.out.println(Arrays.toString(lol));
+
+    }
+
+    private static int[] mergesort(int[] arr) {
+        return null;
+    }
+}
