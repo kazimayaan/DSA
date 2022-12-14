d954bebd36dd01d436e51582e37672afa687d8e6
kazimayaan <kazimayaan486@gmail.com>
2022-12-14 12:24:42 +0530

    qs

--- recursion/quicksort.java
+++ recursion/quicksort.java
@@ -1,12 +1,18 @@
 package recursion;
 
+import java.util.Arrays;
+
 public class quicksort {
     public static void main(String[] args) {
-        
+    int[] arr= {5,4,3,2,1};
+    qsort(arr,0,arr.length-1);
+    System.out.println(Arrays.toString(arr));
     }
     public static void qsort(int[] arr, int low, int hi){
 
-        
+        while(low<hi){
+            
+        }
 
     }
 }
