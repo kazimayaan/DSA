47a8ae43d0eb6e48c384d7928c6e061463abfbdf
kazimayaan <kazimayaan486@gmail.com>
2022-12-14 12:36:26 +0530

    w

--- recursion/quicksort.java
+++ recursion/quicksort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class quicksort {
     public static void main(String[] args) {
-    int[] arr= {5,4,3,2,1};
+    int[] arr= {5,4,3,2,1,876};
     qsort(arr,0,arr.length-1);
     System.out.println(Arrays.toString(arr));
     }
