6063d7473ca82ffb0c9f91a012cd8c74e2713192
kazimayaan <kazimayaan486@gmail.com>
2022-12-14 12:29:20 +0530

    wr

--- recursion/quicksort.java
+++ recursion/quicksort.java
@@ -11,7 +11,29 @@ public class quicksort {
     public static void qsort(int[] arr, int low, int hi){
 
         while(low<hi){
-            
+            int s = low;
+            int e = hi;
+            int m = s+(e-s)/2;
+            int pivot = arr[m];
+            while(s<e){
+                while(arr[s]<pivot){
+                    s++;
+                    
+                }
+                while(arr[e]>pivot){
+                    e--;
+
+                }
+                if(s<=e){
+                    int temp = arr[s];
+                    arr[s]=arr[m];
+                    arr[m]=temp;
+                    s++;
+                    e--;
+                }
+            }
+            qsort(arr,low,e);
+            qsort(arr,s,hi);
         }
 
     }
