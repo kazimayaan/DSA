94fba7b85ee41d41bcf02c0331b2036cf17eab75
kazimayaan <kazimayaan486@gmail.com>
2023-01-01 09:56:31 +0530

    permutatuions

--- recursion/subsetq/permutation/perm.java
+++ recursion/subsetq/permutation/perm.java
@@ -1,8 +1,11 @@
 package recursion.subsetq.permutation;
 
+import java.util.ArrayList;
+
 public class perm {
     public static void main(String[] args) {
         perm("","abc");
+      System.out.println(permu("","abc"));  
     }
 
     private static void perm(String p, String up) {
@@ -28,4 +31,35 @@ public class perm {
         }
 
     }
+    private static ArrayList<String> permu(String p, String up) {
+
+        if(up.isEmpty()){
+                ArrayList<String> ans = new ArrayList<>();
+            // System.out.println(p);
+            ans.add(p);
+
+            return ans;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> ansa = new ArrayList<>();
+        for(int i = 0; i <= p.length();i++){
+
+
+
+
+            String l = p.substring(0,i);
+            String r = p.substring(i, p.length());
+         
+            ansa.addAll(permu(l+ch+r, up.substring(1)));
+
+
+
+        }
+        return ansa;
+
+    }
+
+
+    
+
 }
