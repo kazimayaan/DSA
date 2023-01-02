7291488bdfa4a57fef176b220803ed83416e6126
kazimayaan <kazimayaan486@gmail.com>
2023-01-02 17:20:57 +0530

    solution

---
+++ recursion/subsetq/k.java
@@ -0,0 +1,21 @@
+package recursion.subsetq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class k {
+    public static void main(String[] args) {
+       List<String> lol = lol("");
+       System.out.println(lol);
+    }
+
+    private static List<String> lol(String string) {
+
+
+        List<String> ll = new ArrayList<>();
+        return ll;
+        
+
+
+    }
+}
