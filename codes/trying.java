a42817030dcc49ca5c1e5b8f3bd185da68e96655
kazimayaan <kazimayaan486@gmail.com>
2023-03-22 13:08:34 +0530

    going back up

---
+++ codes/trying.java
@@ -0,0 +1,10 @@
+package codes;
+import java.util.Arrays;
+
+public class trying {
+    public static void main(String[] args) {
+        String[] strs = {"aaa", "dadsj"};
+        System.out.println(Arrays.toString(strs));
+        
+    }
+}
